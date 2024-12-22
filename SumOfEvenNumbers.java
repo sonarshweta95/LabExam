8e099edfb838935dc83853d6f9adb8ee95a18c19
MPRANU <morepranali986@gmail.com>
2024-12-22 09:49:13 +0530

    File Inserted

---
+++ SumOfEvenNumbers.java
@@ -0,0 +1,33 @@
+//Java Program to print the sum of first 100 even numbers using a for loop 
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i * 2;
+        }
+        System.out.println("The sum of first 100 even numbers is: " + sum);
+    }
+}
+
+
+
+
+
+
+// git config --global user.name "Your Name"
+// git config --global user.email "your.email@example.com"
+// git init
+// git status
+// git add <filename>
+// git add .
+// git commit -m "Your commit message"
+// git log
+// git branch <branch-name>
+// git checkout <branch-name>
+// Create and switch to a new branch in one command:-git checkout -b <branch-name>
+// git merge <branch-name>
+// git push origin <branch-name>
+// git pull origin <branch-name>
+// git clone <repository-url>
+// List all configured remotes:- Add a remote repository: - git remote add origin <repository-url>
+// git remote -v
